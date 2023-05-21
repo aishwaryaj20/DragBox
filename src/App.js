d0bb9eb82c43abde8cfffb1dc53708352586e530
aishwaryaj20 <aishwarya.jadhav0649@gmail.com>
2023-05-21 11:39:15 +0530

    Done

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import "./App.css";
+import { useDrag, useDrop } from "react-dnd";
+
+function Card() {
+  const [{ isDragging }, dragRef] = useDrag(() => ({
+    type: "card",
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  }));
+  return (
+    <div
+      className="card"
+      ref={dragRef}
+      style={{
+        backgroundColor: isDragging ? "#fbb" : "palegoldenrod",
+      }}
+    >
+      ListOne
+    </div>
+  );
+}
+
+function Box({ card, moveCard }) {
+  const [{ isOver }, dropRef] = useDrop(() => ({
+    accept: "card",
+    drop: () => moveCard(),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+    }),
+  }));
+  return (
+    <div
+      className="box"
+      ref={dropRef}
+      style={{ backgroundColor: isOver ? "#bbf" : "rgba(0,0,0,.12" }}
+    >
+      {card ? <Card /> : "List"}
+    </div>
+  );
+}
+
+function App() {
+  const [index, setIndex] = useState(1);
+
+  function moveCard(i) {
+    setIndex(i);
+  }
+
+  return (
+    <div className="app">
+      <Box card={index === 1} moveCard={moveCard.bind(null, 1)}></Box>
+      <Box card={index === 2} moveCard={moveCard.bind(null, 2)}></Box>
+      <Box card={index === 3} moveCard={moveCard.bind(null, 3)}></Box>
+    </div>
+  );
+}
+
+export default App;
